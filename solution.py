d9130bc405c1f4a2bb8d67841fd65be9c2096371
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-10-02 23:10:15 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,4 @@
 class Solution:
     def uniqueOccurrences(self, arr):
-        c1 = Counter(arr)
-        c2 = Counter(c1.values())
-        for x in c2:
-            if(c2[x] > 1):
-                return(False)
-
-        return(True)
+        c = Counter(arr)
+        return len(c.values()) == len(set(c.values()))
